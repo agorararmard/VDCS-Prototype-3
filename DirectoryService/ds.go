b1fb55e9681032d341265922909690fa956c5b32
giko9 <giko97@aucegypt.edu>
2019-11-17 15:43:44 +0200

    Directory Service

--- DirectoryService/ds.go
+++ DirectoryService/ds.go
@@ -15,7 +15,7 @@ import (
 //test the directory service
 //sending tokens was not tested
 func main() {
-	createFile()
+	createEmptyDS()
 	id := 0
 	var k RegisterationMessage
 	k.Server.IP = []byte("192.168.1.1")
@@ -23,24 +23,25 @@ func main() {
 	k.Server.PublicKey = []byte("abcsjsjsa")
 	k.Server.NumberOfGates = 321
 	k.Server.FeePerGate = 256.55
-	k.Type = "Client"
-	writeServer(k, &id)
-	writeServer(k, &id)
-	var k2 ClientInfo
-	k2.IP = []byte("192.168.1.1")
-	k2.Port = 3030
-	k2.PublicKey = []byte("abcsjsjsa")
-	writeClient(k2)
-	writeClient(k2)
-	k2.IP = []byte("192.168.3.1")
-	k2.Port = 3030
-	k2.PublicKey = []byte("abjsjsa")
-	writeClient(k2)
+	k.Type = "Server"
+	writeToDS(k, &id)
+	writeToDS(k, &id)
+	var k2 RegisterationMessage
+	k2.Server.IP = []byte("192.168.1.1")
+	k2.Server.Port = 3030
+	k2.Server.PublicKey = []byte("abcsjsjsa")
+	k2.Type = "Client"
+	writeToDS(k2, &id)
+	writeToDS(k2, &id)
+	k2.Server.IP = []byte("192.168.3.1")
+	k2.Server.Port = 3030
+	k2.Server.PublicKey = []byte("abjsjsa")
+	writeToDS(k2, &id)
 
 }
 
 //read Directory Service word by word
-func read() []string {
+func readFromDS() []string {
 	file, err := os.Open("DirectoryService.txt")
 	if err != nil {
 		log.Fatal(err)
@@ -78,51 +79,86 @@ func validNewServer(lines []string, k ServerInfo) bool {
 
 }
 
-//check if a registered server sends his correct information
-func validRegisteredServer(lines []string, k ServerInfo, t Token) bool {
-	for i := 1; i < len(lines); i += 7 {
-		if lines[i-1] == "Server" && lines[i] == string(k.IP) && lines[i+2] == string(k.PublicKey) && lines[i+5] == string(t.TokenGen) {
-			return true
+//check if a registered User sends his correct information
+func validRegisteredUser(lines []string, k RegisterationMessage, t Token) bool {
+	//case Server
+	if k.Type == "Server" {
+		for i := 1; i < len(lines); i += 7 {
+			if lines[i-1] == "Server" && lines[i] == string(k.Server.IP) && lines[i+2] == string(k.Server.PublicKey) && lines[i+5] == string(t.TokenGen) {
+				return true
+			}
+
 		}
+		return false
+	} else {
+		//case Client
+		for i := 1; i < len(lines); i += 7 {
+			if lines[i-1] == "Client" && lines[i] == string(k.Server.IP) && lines[i+2] == string(k.Server.PublicKey) && lines[i+5] == string(t.TokenGen) {
+				return true
+			}
+
+		}
+		return false
 
 	}
-	return false
 
 }
 
-//write new server to the directory service
-func writeServer(k RegisterationMessage, id *int) {
-	if validNewServer(read(), k.Server) {
-		token := string(k.Server.IP) + strconv.FormatInt(int64(k.Server.Port), 10) + string(k.Server.PublicKey) + strconv.FormatInt(int64(k.Server.NumberOfGates), 10) + strconv.FormatFloat(k.Server.FeePerGate, 'f', 6, 64)
-		token = strconv.FormatInt(int64(*id), 10) + generateToken(token)
-		*id++
-		f, err := os.OpenFile("DirectoryService.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if _, err := f.Write([]byte("Server " + string(k.Server.IP) + " " + strconv.FormatInt(int64(k.Server.Port), 10) + " " + string(k.Server.PublicKey) + " " + strconv.FormatInt(int64(k.Server.NumberOfGates), 10) + " " + strconv.FormatFloat(k.Server.FeePerGate, 'f', 6, 64) + " " + string(token) + "\n")); err != nil {
-			log.Fatal(err)
-		}
-		if err := f.Close(); err != nil {
-			log.Fatal(err)
-		}
-		var t Token
-		t.TokenGen = []byte(token)
-		//send token to the server
-		if k.Type == "Client" {
-			GetFromClient(t, k.Server.IP, k.Server.Port)
-		} else {
+//Write to Directory Service
+func writeToDS(k RegisterationMessage, id *int) {
+	//write new server to the directory service
+	if k.Type == "Server" {
+		if validNewServer(readFromDS(), k.Server) {
+			token := string(k.Server.IP) + strconv.FormatInt(int64(k.Server.Port), 10) + string(k.Server.PublicKey) + strconv.FormatInt(int64(k.Server.NumberOfGates), 10) + strconv.FormatFloat(k.Server.FeePerGate, 'f', 6, 64)
+			token = strconv.FormatInt(int64(*id), 10) + generateToken(token)
+			*id++
+			f, err := os.OpenFile("DirectoryService.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if _, err := f.Write([]byte("Server " + string(k.Server.IP) + " " + strconv.FormatInt(int64(k.Server.Port), 10) + " " + string(k.Server.PublicKey) + " " + strconv.FormatInt(int64(k.Server.NumberOfGates), 10) + " " + strconv.FormatFloat(k.Server.FeePerGate, 'f', 6, 64) + " " + string(token) + "\n")); err != nil {
+				log.Fatal(err)
+			}
+			if err := f.Close(); err != nil {
+				log.Fatal(err)
+			}
+			var t Token
+			t.TokenGen = []byte(token)
 			GetFromServer(t, k.Server.IP, k.Server.Port)
-		}
-		println("New Server been registered")
+			println("New Server been registered")
 
-	} else {
-		println("Server Has already been registered")
+		} else {
+			println("Server Has already been registered")
+		}
+	} else if k.Type == "Client" { //write new Client to the directory service
+		if validNewClient(readFromDS(), k.Server) {
+			token := string(k.Server.IP) + strconv.FormatInt(int64(k.Server.Port), 10) + string(k.Server.PublicKey) + strconv.FormatInt(int64(k.Server.NumberOfGates), 10) + strconv.FormatFloat(k.Server.FeePerGate, 'f', 6, 64)
+			token = strconv.FormatInt(int64(*id), 10) + generateToken(token)
+			*id++
+			f, err := os.OpenFile("DirectoryService.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if _, err := f.Write([]byte("Client " + string(k.Server.IP) + " " + strconv.FormatInt(int64(k.Server.Port), 10) + " " + string(k.Server.PublicKey) + " NULL NULL " + string(token) + "\n")); err != nil {
+				log.Fatal(err)
+			}
+			if err := f.Close(); err != nil {
+				log.Fatal(err)
+			}
+			var t Token
+			t.TokenGen = []byte(token)
+			GetFromClient(t, k.Server.IP, k.Server.Port)
+			println("New Client been registered")
+
+		} else {
+			println("Client Has already been registered")
+		}
+
 	}
 }
 
 //check if new Client is valid or has already been registered
-func validNewClient(lines []string, k ClientInfo) bool {
+func validNewClient(lines []string, k ServerInfo) bool {
 	for i := 1; i < len(lines); i += 7 {
 		if lines[i-1] == "Client" {
 			if lines[i] == string(k.IP) {
@@ -134,38 +170,6 @@ func validNewClient(lines []string, k ClientInfo) bool {
 	}
 	return true
 
-}
-
-//check if a registered Client sends his correct information
-func validRegisteredClient(lines []string, k ClientInfo) bool {
-	for i := 1; i < len(lines); i += 7 {
-		if lines[i-1] == "Client" && lines[i] == string(k.IP) && lines[i+2] == string(k.PublicKey) {
-			return true
-		}
-
-	}
-	return false
-
-}
-
-//write new Client to the directory service
-func writeClient(k ClientInfo) {
-	if validNewClient(read(), k) {
-		f, err := os.OpenFile("DirectoryService.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if _, err := f.Write([]byte("Client " + string(k.IP) + " " + strconv.FormatInt(int64(k.Port), 10) + " " + string(k.PublicKey) + " NULL NULL NULL\n")); err != nil {
-			log.Fatal(err)
-		}
-		if err := f.Close(); err != nil {
-			log.Fatal(err)
-		}
-		println("New Client been registered")
-
-	} else {
-		println("Client Has already been registered")
-	}
 }
 
 //break string to array of characters
@@ -201,7 +205,7 @@ func generateToken(str string) string {
 }
 
 //create Empty DirectorySevice
-func createFile() {
+func createEmptyDS() {
 
 	var file, err = os.Create("DirectoryService.txt")
 	if err != nil {
